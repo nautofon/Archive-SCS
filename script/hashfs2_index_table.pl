780c7c13eb04b0b5549ddc334a4c90897c3e47c7
nautofon <129151465+nautofon@users.noreply.github.com>
2025-09-24 02:03:47 +0200

    Improve HashFS v2 index table script
    - Show the kind of all three header parts in the table
    - Show entry pathnames in the table
    - Add --grep option
    - Change --index to output hex, add --raw for the old behavior
    - Document CLI options

--- script/hashfs2_index_table.pl
+++ script/hashfs2_index_table.pl
@@ -66,6 +66,20 @@ sub raw_index ($mount, $index) {
 }
 
 
+sub print_index ($mount, $index) {
+  my $raw = raw_index $mount, $index;
+
+  require Data::Hexdumper;
+  Data::Hexdumper->VERSION('3.00');
+  my $format = '%6a: %8S>  %d';
+  $index == 1 and $format = '%6a: %Q> %L> %2S>  %d';
+  $index == 2 and $format = '%6a: %4L>  %d';
+  print Data::Hexdumper::hexdump( $raw, { output_format => $format } )
+    =~ s{^0x(.+)  }{ lc "$1  " }egmr;
+  # The byte order of this output matches the physical file, not the logical data
+}
+
+
 sub print_index_table ($mount, $select_entries = undef) {
   my %index_offsets;
   my $index1 = raw_index $mount, 1;
@@ -84,12 +98,23 @@ sub print_index_table ($mount, $select_entries = undef) {
     @entries = grep { $entry_hash{$_} } @entries;
   }
 
-  say "index1  index2  offset    kind/flags  zsize   usize   hash (netwk ord)  tobj_header";
+  my %names;
+  $names{ cityhash64 $_ } = $_ for $mount->list_files, $mount->list_dirs;
+  $names{ cityhash64 '' } = '/' if exists $names{ cityhash64 '' };
+
+  say 'index1  index2  offset    parts   flags   zsize   usize   hash (netwk ord)  entry name';
   for my $hash ( @entries ) {
     next if $opts{start} && state $start++ < $opts{start} - 1;
     last if $opts{limit} && state $limit++ > $opts{limit} - 1;
 
     my $e = $mount->entry_meta($hash);
+
+    my @parts = $e->{parts}->@*;
+    my $parts =
+      @parts == 1 ? sprintf '%02x    ',     $parts[0]{kind} :
+      @parts == 2 ? sprintf '%02x%02x  ',   $parts[0]{kind}, $parts[1]{kind} :
+      @parts == 3 ? sprintf '%02x%02x%02x', $parts[0]{kind}, $parts[1]{kind}, $parts[2]{kind} :
+      @parts != 0 ? sprintf '%02x #%-2i',   $parts[0]{kind}, scalar @parts : '------';
 
     my $tobj_header = $e->{is_tobj} ?
       join ' ', unpack '(H4)4 H8', join '', map $e->{parts}[$_]{header}, 0..1 : '';
@@ -101,20 +126,24 @@ sub print_index_table ($mount, $select_entries = undef) {
 #     my ( $e1, $e2, )
 #       = unpack '(SS)<', $e->{parts}[1]{header};
 
-    say sprintf "%06x  %06x  %08x  %02x/%02x %02x%02x  %06x  %06x  %s  %s",
+    say sprintf "%06x  %06x  %08x  %s  %02x%02x%02x  %06x  %06x  %s  %s",
       $index_offsets{$hash}[1], $index_offsets{$hash}[2], $e->{offset},
-      $e->{parts}[0]{kind}, $e->{flags1}, $e->{flags2}, $e->{flags3},
+      $parts, $e->{flags1}, $e->{flags2}, $e->{flags3},
       $e->{zsize}, $e->{size},
       cityhash64_as_hex $hash,
-      $tobj_header;
+      $names{$hash} // '';
+    say ' ' x 58, 'tobj header: ', $tobj_header if $e->{is_tobj};
   }
 }
 
 
 GetOptions \%opts, qw(
   entry|e=s@
+  grep|E=s@
+  help|?
   index=i
   limit=i
+  raw
   start=i
 );
 pod2usage -verbose => 2 if $opts{help};
@@ -129,16 +158,82 @@ my $mount = $scs->mount( Archive::SCS::HashFS2->new(path => $path) );
 # print_file_header $mount; exit;
 
 
-if ($opts{index}) {
+if ($opts{index} && $opts{raw}) {
   print raw_index $mount, $opts{index};
   exit;
 }
+if ($opts{index}) {
+  print_index $mount, $opts{index};
+  exit;
+}
 
-print_index_table $mount, $opts{entry};
+
+if ($opts{entry} || $opts{grep}) {
+  my @entries;
+  if ($opts{grep}) {
+    @entries = $mount->list_files, $mount->list_dirs;
+    for my $grep ( $opts{grep}->@* ) {
+      @entries = grep { m/$grep/ } @entries;
+    }
+    @entries or warn 'No entries matched with --grep';
+  }
+  push @entries, ( $opts{entry} // [] )->@*;
+
+  print_index_table $mount, \@entries;
+}
+else {
+  print_index_table $mount;
+}
 
 
 =head1 SYNOPSIS
 
   script/hashfs2_index_table.pl dlc_ks.scs
   script/hashfs2_index_table.pl dlc_ks.scs -e 'material/ui/dlc/dlc_ks.tobj'
-  script/hashfs2_index_table.pl dlc_ks.scs --index 2 | xxd > index2.txt
+  script/hashfs2_index_table.pl dlc_ks.scs -E 'garden_city.*rainbow'
+  script/hashfs2_index_table.pl dlc_ks.scs --index 2 > index2.txt
+  script/hashfs2_index_table.pl --help
+
+=head1 OPTIONS
+
+=over
+
+=item --entry, -e
+
+Limit the output table to the given entries only. Can be given
+multiple times. Accepts entry pathnames or hash values
+(16-byte hex string in network byte order).
+
+=item --grep, -E
+
+Limit the output table to only those entries that have a pathname
+matching the given regular expression. If given multiple times,
+each regex further restricts the entry selection (like multiple
+piped C<grep>s).
+
+=item --help, -?
+
+Display this manual page.
+
+=item --index
+
+Instead of printing the composite table, print a simple ASCII hex
+view of the uncompressed index 1 or index 2. See also C<--raw>.
+
+=item --limit
+
+Limit the output table to at most the given number of entries.
+
+=item --raw
+
+Modifies C<--index> to spew the raw uncompressed binary instead of
+printing the ASCII view of an index. Useful for dumping the index
+into a file and viewing in your hex editor of choice, or when you
+don't have L<Data::Hexdumper> installed.
+
+=item --start
+
+Start the output table at the entry with the given number instead
+of the first entry.
+
+=back

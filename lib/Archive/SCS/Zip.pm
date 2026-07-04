6f1533b897d23ae6da15cb37784efccda41eb4f8
nautofon <129151465+nautofon@users.noreply.github.com>
2026-07-04 07:02:12 +0200

    Update Changes, bump version to 1.11

--- lib/Archive/SCS/Zip.pm
+++ lib/Archive/SCS/Zip.pm
@@ -2,7 +2,7 @@ use v5.34;
 use warnings;
 use Object::Pad 0.73;
 
-class Archive::SCS::Zip 1.10
+class Archive::SCS::Zip 1.11
   :isa( Archive::SCS::Mountable );
 
 use builtin::compat qw( blessed true );
